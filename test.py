2348e14c90001f87ded2819467206726d8278777
Damir <38811598+MrDemSay@users.noreply.github.com>
2020-09-10 21:27:50 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
 import matplotlib.pyplot as plt
-plt.plot([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
+
+# Линейная зависимость
+x = np.linspace(0, 10, 50)
+y1 = x
+# Квадратичная зависимость
+y2 = [i**2 for i in x]
+# Построение графика
+plt.title("Зависимости: y1 = x, y2 = x^2") # заголовок
+plt.xlabel("x")         # ось абсцисс
+plt.ylabel("y1, y2")    # ось ординат
+plt.grid()              # включение отображение сетки
+plt.plot(x, y1, x, y2)  # построение графика
+
 plt.show()
