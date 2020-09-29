cfe5e53a83f4ec73b112f6e95f51b461f955b703
Damir <38811598+MrDemSay@users.noreply.github.com>
2020-09-29 10:02:53 +0400

    Update MM_labs_1.py

--- Lab1/MM_labs_1.py
+++ Lab1/MM_labs_1.py
@@ -4,96 +4,112 @@ import numpy as np
 file = open('data.txt', 'w')
 
 
-def func1(h, q, i, p):
+def Eilers_method():
+	file = open('EILERdata.txt', 'w')
 
 	h = 0.2
-	pogr = 0
-	q = 0
+	x = 0
 	i = 0
-	p = 1.5
+	p = 1.5 
 	Y = [1.5]
 	YG = []
 	YM = []
 	X = [0]
 
 	while (i <= 3):
-		q = q + 0.2
-		y = p + h * (p-q)
-		yg = 0.5 * math.exp(q) + q + 1
-		ym = p + h/2 * ((p-q) + ((h * (p-q) + p - (q+0.2))))
+		x = x + 0.2
+		y = p + h * (p-x)
+		yg = 0.5 * math.exp(x) + x + 1
+		ym = p + h/2 * ((p-x) + ((h * (p-x) + p - (x+0.2))))
 		
-		p = y
+		p = y #переопределение значения
 
-		X.append(round(q, 3))
-		Y.append(round(y, 3))
-		YG.append(round(yg, 3))
-		YM.append(round(ym, 3))
+
+		#recording in arrays
+		#X.append(round(x, 3)) 
+		#Y.append(round(y, 3))
+		#YG.append(round(yg, 3))
+		#YM.append(round(ym, 3))
 
 		#recording in file 
 		file.write(str(round(y, 3)))
 		file.write(' ')
-		file.write(str(round(q, 3)))
+		file.write(str(round(x, 3)))
 		file.write(' ')
 		file.write(str(round(yg, 3)))
 		file.write(' ')
-		file.write(str(round(q, 3)))
+		file.write(str(round(x, 3)))
 		file.write(' ')
 		file.write(str(round(ym, 3)))
 		file.write(' ')
-		file.write(str(round(q, 3)))
+		file.write(str(round(x, 3)))
 		file.write(' ')
 		file.write('\n')
 
 		i = i + 1
 
-def RungeKutta():
 
-	Y_ = [1]
-	X_ = [0]
-	K_ = []
+
+
+def RungeKutta_Method():
+	file = open('RKdata.txt', 'w')
+
+	h = 0.1
+	x = 0
 	i = 0
-		
+	y = 0
+	p = 1 #y_prev
+	e = 1
+	k_array = []
+	y_array = []
 
-	H = 0.1
-
-	while(i < 4):
+	while (i <= 3 and x <= 1):
 
 		if i == 0:
-			K_[i] = H * (2 * (X_[i] ** 2 + Y_[i]))
-			K_.append(K_[i])
-			
-		elif(i > 0 and len(K_) < 3):
-			X_[i] = X_[x-1] + H
-			K_[i] = H * ((X_[i] + H/2) ** 2 + Y_[i] + K_[i-1]/2)
-			K_.append(K_[i])
+			k = h * (2 * (x ** 2 + p))
+		elif i > 0 and i < 3:
+			y = p + 1/6 * sum_k(k_array)
+			y_array.append(y)
+			k = h * (2 *((x + h/2) ** 2 + y + k_prev/2))
 		else:
-			K_[i] = H * 2 * ((X_[i]+H)**2 + Y_[i]+K_[i-1])
-			K_.append(K_[i])
-
+			y = p + 1/6 * sum_k(k_array)
+			k = h * ((x + h) ** 2 + y + k_prev)
 
 		
-		
-		X_.append(X_[i])
+		k_array.append(k)
+		def sum_k(k_array):
+			suma = 0
+			for i in k_array:
+				suma += i
+			return suma
 
-		Y_[i+1] = Y_[i] + 1/6*(K_[0] + 2*K_[1] + 2*K_[2] + K_[3])
-		Y_.append(Y_[i+1])
+		print(sum_k(k_array))
+		print("y-array = ",y_array)
 
 
+		#recording in file
+		file.write(str(round(k, 3)))
+		file.write(' ') 
+		file.write(str(round(x, 3)))
+		file.write(' ')
+		file.write(str(round(p, 3)))
+		file.write(' ')
+		file.write('\n')
+
+		p = y
+		k_prev = k
+		x = x + h
 		i = i + 1
-		
 
-		
+	print(k_array)
 
-		
+#Eilers_method()
 
+RungeKutta_Method()
 
-
-#func1(h, q, i, p)
-
-RungeKutta()
-print("X_ARRAY: ", X_, end="\n")
-print("Y_ARRAY: ", Y_, end="\n")
-print("K_ARRAY: ", K_, end="\n")
+#print("X_ARRAY: ", X_, end="\n")
+#print("Y_ARRAY: ", Y_, end="\n")
+#print("K_ARRAY: ", K_, end="\n")
 
 
 #arrays
