29d797f98de275dcfa4cf78a3a53e54042ced965
Damir <38811598+MrDemSay@users.noreply.github.com>
2020-09-30 07:58:07 +0400

    Update algoritms.py

--- algoritms.py
+++ algoritms.py
@@ -22,4 +22,18 @@ def selection_sort(array):
         array[i], array[low_value_index] = array[low_value_index], array[i]
     print(array)
 
-selection_sort(array)
+#selection_sort(array)
+
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        item_to_insert = array[i]
+        j = i - 1
+
+        while j >= 0 and array[j] > item_to_insert:
+            array[j + 1] = array[j]
+            j -= 1
+
+        array[j + 1] = item_to_insert
+    print(array)
+
+#insertion_sort(array)
